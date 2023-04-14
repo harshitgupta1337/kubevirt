ecc2cb76268f8ea7d2362e3cfaffcc7d998ed2a1
Harshit Gupta <guptaharshit@microsoft.com>
2023-04-14 09:59:58 -0700

    Adding instructions to create a CH VM on virt-launcher image

---
+++ ch-integration/vm-on-virt-launcher-ctr/main.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+docker build -t virt-launcher:test \
+    -f $SCRIPT_DIR/virt-launcher-test-image/Dockerfile $SCRIPT_DIR/virt-launcher-test-image/
+
+$SCRIPT_DIR/create-prereq-files.sh
+
+ctr=$(docker run -d --privileged virt-launcher:test)
+
+# Copy the needed files into the container
+$SCRIPT_DIR/copy-files-to-docker.sh $ctr
+
+# Now run the VM in the container
+docker exec -it $ctr /launch-ch-vm.sh
