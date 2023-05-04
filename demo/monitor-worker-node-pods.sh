eed9b53f66fd4ff523b9ad400069ade73ee96611
Harshit Gupta <guptaharshit@microsoft.com>
2023-05-03 18:19:57 -0700

    demo script changes

--- demo/monitor-worker-node-pods.sh
+++ demo/monitor-worker-node-pods.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-wkr_node=$(kubectl get nodes -l '!node-role.kubernetes.io/control-plane' -o jsonpath='{.items[*].metadata.name}'); watch -n1 kubectl get pods -A --field-selector spec.nodeName=$wkr_node
+wkr_node=$(kubectl get nodes -l '!node-role.kubernetes.io/control-plane' -o jsonpath='{.items[*].metadata.name}')
+
+watch -n1 kubectl get pods -A --field-selector spec.nodeName=$wkr_node
