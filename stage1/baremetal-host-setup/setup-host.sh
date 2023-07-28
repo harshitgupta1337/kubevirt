359ae9eac985712c0bdf10ddd4a18518ca84f090
Harshit Gupta <guptaharshit@microsoft.com>
2023-07-28 15:28:22 -0700

    Add setup-host.sh

---
+++ stage1/baremetal-host-setup/setup-host.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+sudo tdnf -y install mariner-repos-extended.noarch
+sudo tdnf -y install mariner-repos-preview
+sudo tdnf -y update
+
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P OUTPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+
+sudo tdnf install -y ms-kubernetes-*
+
+sudo tdnf install -y conntrack
+
+sudo modprobe br_netfilter
+
+sudo kubeadm init --config kubeadm-config.yaml
+
+mkdir .kube
+sudo cp /etc/kubernetes/admin.conf .kube/config
+sudo chown -R $USER .kube
+
+kubectl get pods -A
+
+kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/v0.19.2/Documentation/kube-flannel.yml
+
+kubectl taint node hermes-new-loader node-role.kubernetes.io/control-plane-
